444ae59e0cd63c05ea85d289524f9d7b25dca9ed
Halil Enes <halil.enes.ozdemir@gmail.com>
2022-06-20 20:04:11 +0300

    Environment variables lesson completed( Lesson 2)

--- protoreactproje/src/App.js
+++ protoreactproje/src/App.js
@@ -4,13 +4,20 @@ function App() {
   return (
     <>
       <div className='App'>test</div>
+      <h3>{process.env.NODE_ENV}</h3>
+      <p>{process.env.REACT_APP_API_URL}</p>
       {/* Logo png dosyası static bir dosya ve publicde bulunuyor, direkt çağırabilirim favicon.ico gibi 
       Fakat logo.svg' publicde olmadığı için import etmem ve kullanmam gerekiyor.
       */}
       {/*  GENERATE_SOURCEMAP= false ile birlikte build edildikten sonraki kodları herkesin görememesini sağlayabiliriz. */}
 
-      <img src='./logo192.png' alt='' />
-      <img src={logo} alt='' />
+      {/* Production ortamında image'lar gözüksün, development'ta gözükmesin istiyorsak... */}
+      {process.env.NODE_ENV === 'production' && (
+        <>
+          <img src='./logo192.png' alt='' />
+          <img src={logo} alt='' />
+        </>
+      )}
     </>
   );
 }
