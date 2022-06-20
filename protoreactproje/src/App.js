1d3d90609517fb81620da23c369444032358e704
Halil Enes <halil.enes.ozdemir@gmail.com>
2022-06-20 20:24:29 +0300

    Styled-Components general knowledge understood.

--- protoreactproje/src/App.js
+++ protoreactproje/src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import Test from './Test';
 import styles from './App.module.css';
+import { Title } from './Components';
+
 function App() {
   return (
     <>
       <div className={styles.App}>
-        <h3>{process.env.NODE_ENV}</h3>
+        <Title>{process.env.NODE_ENV}</Title>
+        <Title theme='dark'>{process.env.NODE_ENV}</Title>
         <p>{process.env.REACT_APP_API_URL}</p>
       </div>
       <Test />
