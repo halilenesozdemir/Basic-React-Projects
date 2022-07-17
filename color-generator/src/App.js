537f7628b2326f72c647c8d91e001fda295c1d52
Halil Enes <halil.enes.ozdemir@gmail.com>
2022-07-17 06:41:19 +0300

    Color values and error part handled.

--- color-generator/src/App.js
+++ color-generator/src/App.js
@@ -10,7 +10,13 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('Hello');
+    try {
+      let colors = new Values(color).all(10);
+      console.log(colors);
+    } catch (error) {
+      setError(true);
+      console.log(error);
+    }
   };
 
   return (
@@ -18,7 +24,16 @@ function App() {
       <section className='container'>
         <h3>Color Generator</h3>
         <form onSubmit={handleSubmit}>
-          <input type='text' value={color} onChange={(e) => setColor(e.target.value)} placeholder='#f15025' />
+          <input
+            type='text'
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+            placeholder='#f15025'
+            className={`${error ? 'error' : null}`}
+          />
+          <button className='btn' type='submit'>
+            submit
+          </button>
         </form>
       </section>
       <section className='colors'>
